68c8fe939dc17ce2c591165d4f9cfe64281a70e5
larvyde <larvyde@gmail.com>
2021-11-13 11:23:46 +0700

    carousel

---
+++ tiedwidjes/index.js
@@ -0,0 +1,16 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    var carouselItems = [
+        '<div class="carousel-item"><img src="carousel/carousel-01.jpg" /></div>',
+        '<div class="carousel-item"><img src="carousel/carousel-01.jpg" /></div>',
+        '<div class="carousel-item"><img src="carousel/carousel-01.jpg" /></div>',
+        '<div class="carousel-item"><img src="carousel/carousel-01.jpg" /></div>',
+        '<div class="carousel-item"><img src="carousel/carousel-01.jpg" /></div>',
+        '<div class="carousel-item"><img src="carousel/carousel-01.jpg" /></div>'
+    ]
+
+    var carousel = new SwiperBox({ items: carouselItems })
+    carousel.HTMLElement.classList.add("carousel")
+    document.getElementById("carousel").appendChild(carousel.HTMLElement);
+})
+*/
